4c8193c6575e24d8f73922cefaf431292969e3bb
Frikk Balder <33499052+MindChirp@users.noreply.github.com>
2024-11-19 00:30:58 +0100

    Moved files

---
+++ old-cnn-model/trainer.py
@@ -0,0 +1,62 @@
+from model import ImageNetwork
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import torchvision
+
+ 
+
+
+# Define transforms for the training set. This is important because minimizing the image sizes helps speed up training.
+transforms = transforms.Compose([
+  transforms.Resize((350, 350)),
+  transforms.ToTensor(),
+])
+
+# Load the training set
+train_dataset = torchvision.datasets.ImageFolder(root='train', transform=transforms)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print(train_loader.dataset)
+
+model = ImageNetwork().to(device)
+criterion = nn.BCEWithLogitsLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+num_epochs = 20
+for epoch in range(num_epochs):
+  running_loss = 0.0
+  for i, data in enumerate(train_loader, 0):
+      inputs, labels = data
+
+      inputs, labels = inputs.to(device), labels.to(device)
+
+      # Convert labels to one-hot encoding
+      labels_one_hot = torch.zeros(labels.size(0), 8).to(device)
+      labels_one_hot.scatter_(1, labels.unsqueeze(1), 1)
+
+      # Zero the pareaameters gradients
+      optimizer.zero_grad()
+      
+      # Forward pass
+      outputs = model(inputs)
+      loss = criterion(outputs, labels_one_hot)
+
+      # Backward pass and optimize
+      loss.backward()
+      optimizer.step()
+
+      # Print statistics
+      running_loss += loss.item()
+      if i % 100 == 99:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Step [{i + 1}], Loss: {running_loss/100:.4f}')
+        running_loss = 0.0
+
+
+torch.save(model.state_dict(), 'model.pth')
+print('Finished Training')
--- run_yolo.py
+++ run_yolo.py
@@ -1,4 +1,4 @@
-from model import ImageNetwork
+from gymnasium import spaces
 import torch
 from mss import mss
 from PIL import Image
@@ -11,47 +11,28 @@ from ultralytics import YOLO
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = YOLO("runs/detect/train44/weights/best.pt").to(device)
-
 print(device)
-# Set up the image model
-# model = ImageNetwork()
-# model.load_state_dict(torch.load('model.pth'))
-# model.eval()
-# model.to(device)
-# model.to(device)
 
-
+name_map = [ 
+ 'Deepslate Diamond Ore', 'Deepslate Gold Ore', 'Deepslate Iron Ore', 'Deepslate RedstoneOre', 'Iron Ore' 
+]
 
 
 mon = {'left': 160, 'top': 160, 'width': 700, 'height': 700}
 
 with mss() as sct:
   while True:
-    # time.sleep(0.05) 
+    time.sleep(1) 
     screenshot = sct.grab(mon) 
     img = Image.frombytes('RGB', (screenshot.width, screenshot.height), screenshot.rgb)
 
-    # Run the image through the model
-
+    # show=True displays a window with the annotated image
     predicted = model.predict(img, device=device, show=True)
-    # img_bgr = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-    # cv2.imshow('test', img_bgr)
-    # if cv2.waitKey(33) & 0xFF in (
-    #     ord('q'), 
-    #     27, 
-    # ):
-    #     break
+    ores = np.zeros(len(name_map))
 
-    # for result in predicted:
-    #   for box, label in zip(result.boxes, result.names):
-    #     print(f'Label: {label}, box: {box}')
-
-    # with torch.no_grad():
-    #   output = model(image)
-    #   _, predicted = torch.max(output, 1)
-
-
-      
-    # print(f'Predicted class: {name_map[predicted.item()]}')
+    for detection in predicted:
+      boxes = detection.boxes
+      for box in boxes:
+        ores[int(box.cls[0])] = 1
 
 
